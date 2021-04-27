1bfb8f8e91abefad6bac976dda54717860afeae0
Chashmat un Nasim <chashmatnasim@gmail.com>
2021-04-27 20:57:31 +0500

    dsdf

--- js/index.js
+++ js/index.js
@@ -2,26 +2,26 @@ const key = `URkMv8qNAL1ettx3XUnfAsjAafb3t8zS`;
 
 // getting weather
 
-const getWeather = async (id) => {
-  const baseUrl = "http://dataservice.accuweather.com/currentconditions/v1/";
-  const query = `${id}?apikey=${key}`;
+// const getWeather = async (id) => {
+//   const baseUrl = "http://dataservice.accuweather.com/currentconditions/v1/";
+//   const query = `${id}?apikey=${key}`;
 
-  const res = await fetch(baseUrl + query);
-  const data = await res.json();
+//   const res = await fetch(baseUrl + query);
+//   const data = await res.json();
 
-  return data[0];
-  console.log(data)
-};
+//   return data[0];
+//   console.log(data)
+// };
 
 // getting city
-const getCity = async (city) => {
-  const baseUrl =
-    "http://dataservice.accuweather.com/locations/v1/cities/search";
-  const query = `?apikey=${key}&q=${city}`;
+// const getCity = async (city) => {
+//   const baseUrl =
+//     "http://dataservice.accuweather.com/locations/v1/cities/search";
+//   const query = `?apikey=${key}&q=${city}`;
 
-  const res = await fetch(baseUrl + query);
-  const data = await res.json();
+//   const res = await fetch(baseUrl + query);
+//   const data = await res.json();
 
-  return data[0];
-  console.log(data)
-};
+//   return data[0];
+//   console.log(data)
+// };
